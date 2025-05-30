5079d1bebc32f1f23162f2f8bc06dceb2b3efad7
Nguyễn Tiến Đạt <datnt1171@gmail.com>
2025-05-30 22:17:21 +0700

    feat: add translation on login page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,12 +1,15 @@
 import LoginForm from "@/components/login-form"
+import { useTranslations } from "next-intl";
 
 export default function LoginPage() {
+  const t = useTranslations("LoginPage");
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div className="text-center">
-          <h1 className="text-3xl font-bold">Task Management System</h1>
-          <h2 className="mt-6 text-2xl font-bold tracking-tight">Sign in to your account</h2>
+          <h1 className="text-3xl font-bold">{t("title")}</h1>
+          <h2 className="mt-6 text-2xl font-bold tracking-tight">{t("subtitle")}</h2>
         </div>
         <LoginForm />
       </div>
